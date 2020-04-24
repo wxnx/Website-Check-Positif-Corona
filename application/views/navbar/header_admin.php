29890a7c8b069bd5650c4a380393364787909ac3
winsxz <wino99oniw@gmail.com>
2020-04-25 02:10:48 +0700

    update header 	modified:   views/navbar/header_admin.php 	modified:   views/navbar/header_user.php

--- application/views/navbar/header_admin.php
+++ application/views/navbar/header_admin.php
@@ -13,6 +13,10 @@
 
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
 
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
     <title><?php echo $judul ?></title>
 </head>
 
@@ -25,10 +29,10 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <a class="nav-item nav-link" href="#">Beranda</a></li>
-                    <a class="nav-item nav-link" href="#">Validasi Pasien</a></li>
-                    <a class="nav-item nav-link" href="#">Input Hasil Test Pasien</a></li>
-                    <a class="nav-item nav-link" href="<?= site_url('data_penyebaran/tambah_data') ?>">Input Data Persebaran Corona</a></li>
+                    <li><a class="nav-item nav-link" href="#">Beranda</a></li>
+                    <li><a class="nav-item nav-link" href="#">Validasi Pasien</a></li>
+                    <li><a class="nav-item nav-link" href="#">Input Hasil Test Pasien</a></li>
+                    <li><a class="nav-item nav-link" href="<?= site_url('data_penyebaran/tambah_data') ?>">Input Data Persebaran Corona</a></li>
                 </div>
                 <div class="navbar-nav">
                     <li><a class="nav-item nav-link" href="<?= site_url('login/index') ?>">Keluar</a></li>
--- application/views/navbar/header_user.php
+++ application/views/navbar/header_user.php
@@ -7,13 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- MY CSS -->
 
     <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/style.css">
 
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
     <title><?php echo $judul ?></title>
+
 </head>
 
 <body>
@@ -31,7 +35,22 @@
                     <a class="nav-item nav-link" href="<?= site_url('data_penyebaran/index') ?>">Data Persebaran Corona</a></li>
                 </div>
                 <div class="navbar-nav">
-                    <li><a class="nav-item nav-link" href="#">Pengaturan</a></li>
+                    <li>
+                        <div class="dropdown">
+                            <a class="nav-item nav-link" href="#" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Pengaturan
+                            </a>
+
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                <a class="dropdown-item" href="#">Hai,<?php echo $this->session->userdata('nama'); ?></a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Ubah Kecamatan</a>
+                                <a class="dropdown-item" href="#">Ubah Password</a>
+                            </div>
+
+                        </div>
+
+                    </li>
                     <li><a class="nav-item nav-link" href="<?= site_url('login/index') ?>">Keluar</a></li>
                 </div>
             </div>
