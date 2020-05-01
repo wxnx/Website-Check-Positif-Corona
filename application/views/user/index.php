1382903487cca9d049e33bad83c7bb7becba4970
muhammadfarhan26 <45418738+muhammadfarhan26@users.noreply.github.com>
2020-05-01 21:54:59 +0700

    Tambah slider(Carousel)

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -5,10 +5,52 @@
     background-attachment: fixed;
     background-size: 100% 100%;
   }
+  .carousel {
+  margin: auto;
+  width: 60%;
+
+ }
+ .carousel-indicators .active{
+    background-color: #f00;
+}
+.carousel-control-next-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='red' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
+}
+
+.carousel-control-prev-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='red' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+}
 </style>
+
 <div class="container">
   <center>
     <p>Apakah anda ingin melakukan check test Covid-19? Silahkan klik tombol dibawah ini!</p>
     <a class="nav-link" href="<?= site_url('login/daftar') ?>"><button type="button" class="btn btn-warning">Daftar</button></a>
   </center>
+</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://stmikgici.ac.id/wp-content/uploads/2020/03/slider971.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.depok.go.id/storage/uploads/sliders/slider-2020-03-1202:14:18.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.clubsnsw.com.au/sites/default/files/COVID-19%201136x285%20%5BBanner%5D.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
